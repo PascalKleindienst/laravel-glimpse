a366f4c3bbca0652fb2e257fd6eda0a859b78d12
Pascal Kleindienst <mail@pascalkleindienst.de>
2026-03-03 17:55:24 +0100

    test: add archtests

---
+++ tests/Unit/ArchTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
+use LaravelGlimpse\GlimpseServiceProvider;
+use LaravelGlimpse\Resolvers\GeoResolver;
+
+arch()->preset()->php();
+arch()->preset()->laravel();
+arch()->preset()->security();
+
+arch('avoid mutation')
+    ->expect('LaravelGlimpse')
+    ->classes()
+    ->toBeReadonly()
+    ->ignoring([
+        'LaravelGlimpse\Facades',
+        'LaravelGlimpse\Jobs',
+        'LaravelGlimpse\Models',
+        'LaravelGlimpse\Console\Commands',
+        GeoResolver::class,
+        GlimpseServiceProvider::class,
+    ]);
+
+arch('avoid inheritance')
+    ->expect('LaravelGlimpse')
+    ->classes()
+    ->toExtendNothing()
+    ->ignoring([
+        'LaravelGlimpse\Models',
+        'LaravelGlimpse\Console\Commands',
+        'LaravelGlimpse\Jobs',
+        'LaravelGlimpse\Facades',
+        'LaravelGlimpse\Livewire',
+        GlimpseServiceProvider::class,
+    ]);
+
+arch('avoid open for extension')
+    ->expect('LaravelGlimpse')
+    ->classes()
+    ->toBeFinal();
+
+arch('avoid abstraction')
+    ->expect('LaravelGlimpse')
+    ->not->toBeAbstract()
+    ->ignoring([
+        'LaravelGlimpse\Contracts',
+    ]);
+
+arch('factories')
+    ->expect('Database\Factories')
+    ->toExtend(Factory::class)
+    ->toHaveMethod('definition')
+    ->toOnlyBeUsedIn([
+        'LaravelGlimpse\Models',
+    ]);
+
+arch('models')
+    ->expect('LaravelGlimpse\\Models')
+    ->classes()
+    ->toExtend(Model::class);
