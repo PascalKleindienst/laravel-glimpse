2e02f7a50db8dfebc727d057a40eca91822ee9e4
Pascal Kleindienst <mail@pascalkleindienst.de>
2026-03-03 17:50:06 +0100

    chore: add AboutCommand for Glimpse

--- src/GlimpseServiceProvider.php
+++ src/GlimpseServiceProvider.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace LaravelGlimpse;
 
+use Composer\InstalledVersions;
 use hisorange\BrowserDetect\Contracts\ParserInterface;
 use hisorange\BrowserDetect\Parser;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\ServiceProvider;
 use LaravelGlimpse\Console\Commands\InstallGlimpseCommand;
 use Override;
@@ -58,6 +60,12 @@ final class GlimpseServiceProvider extends ServiceProvider
 
         $this->commands([
             InstallGlimpseCommand::class,
+        ]);
+
+        AboutCommand::add('Glimpse Analytics', fn () => [
+            'Version' => InstalledVersions::getPrettyVersion('pascalkleindienst/laravel-glimpse'),
+            'Enabled' => AboutCommand::format(config('glimpse.enabled'), console: fn ($value) => $value ? '<fg=green;options=bold>ENABLED</>' : 'OFF'),
+            'Queue' => AboutCommand::format(config('queue.default', 'sync'), console: fn ($value) => $value !== 'sync' ? $value : '<fg=yellow;options=bold>⚠ sync</>'),
         ]);
     }
 
